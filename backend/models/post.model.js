b9be6acc88a19f270a6c05f43e2458232f3b8d76
airnesygroup <airnesy.info@gmail.com>
2024-12-01 20:55:26 +0300

    second  commit

--- backend/models/post.model.js
+++ backend/models/post.model.js
@@ -6,7 +6,7 @@ const postSchema = new Schema(
     user: {
       type: Schema.Types.ObjectId,
       ref: "User",
-      required: true,
+      required: false,
     },
     img: {
       type: String,
