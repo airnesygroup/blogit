511cac11f837d1c6a89c55c983205f895018eb96
airnesygroup <airnesy.info@gmail.com>
2024-12-01 20:21:45 +0300

    second  commit

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -121,6 +121,39 @@ export const createPost = async (req, res) => {
   }
 };
 
+export const createPost = async (req, res) => {
+  const clerkUserId = req.auth.userId;
+
+  console.log(req.headers);
+
+  if (!clerkUserId) {
+    return res.status(401).json("Not authenticated!");
+  }
+
+  const user = await User.findOne({ clerkUserId });
+
+  if (!user) {
+    return res.status(404).json("User not found!");
+  }
+
+  let slug = req.body.title.replace(/ /g, "-").toLowerCase();
+
+  let existingPost = await Post.findOne({ slug });
+
+  let counter = 2;
+
+  while (existingPost) {
+    slug = `${slug}-${counter}`;
+    existingPost = await Post.findOne({ slug });
+    counter++;
+  }
+
+  const newPost = new Post({ user: user._id, slug, ...req.body });
+
+  const post = await newPost.save();
+  res.status(200).json(post);
+};
+
 export const deletePost = async (req, res) => {
   const clerkUserId = req.auth.userId;
 
