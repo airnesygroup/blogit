a701a885905f5d5f3084caf9d6c7324f0f1910e5
airnesygroup <airnesy.info@gmail.com>
2024-12-03 22:04:46 +0300

    second  commit

--- client/src/routes/Write.jsx
+++ client/src/routes/Write.jsx
@@ -36,7 +36,7 @@ const Write = () => {
   const mutation = useMutation({
     mutationFn: async (newPost) => {
         const token = await getToken();
-        return axios.post(`${import.meta.env.VITE_API_URL}/posts`, newPost, {
+        return axios.post(`${import.meta.env.VITE_API_URL}/api/posts`, newPost, {
           headers: {
             Authorization: `Bearer ${token}`, // Ensure correct format
           
