869f09a07c62a6a3c9ae359e3f4838b42bd9d5f6
airnesygroup <airnesy.info@gmail.com>
2024-12-03 17:37:19 +0300

    second  commit

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import dotenv from "dotenv"; // Ensure dotenv is imported first
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Homepage from "./routes/Homepage.jsx";
 import PostListPage from "./routes/PostListPage.jsx";
@@ -14,7 +13,6 @@ import { ClerkProvider } from "@clerk/clerk-react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-dotenv.config(); // Ensure .env is loaded at the top
 
 const queryClient = new QueryClient();
 
@@ -54,7 +52,7 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ClerkProvider publishableKey={ key}>
+    <ClerkProvider >
       <QueryClientProvider client={queryClient}>
         <RouterProvider router={router} />
         <ToastContainer position="bottom-right" />
