00307d690c4f1248d705318150c07f5725def692
airnesygroup <airnesy.info@gmail.com>
2024-12-06 13:17:33 +0300

    second  commit

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
     <div className="w-full h-16 md:h-20 flex items-center justify-between">
       {/* LOGO */}
       <Link to="/" className="flex items-center gap-4 text-2xl font-bold">
-        <Image src="./logo.png" alt=" Logo" w={32} h={32} />
+        <Image src=".../logo.png" alt=" Logo" w={32} h={32} />
         <span>Hoolicon</span>
       </Link>
       {/* MOBILE MENU */}
